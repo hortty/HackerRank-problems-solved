f63f21f8f07dde644695b207eb045cc4214a21c6
Gabriel Hortmann <gabrielhortmann@gmail.com>
2025-07-22 09:50:08 -0300

    birthday problem

---
+++ _Problems/8 - BirthdayCakeCandles.cs
@@ -0,0 +1,64 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'birthdayCakeCandles' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY candles as parameter.
+     */
+
+    public static int birthdayCakeCandles(List<int> candles)
+    {
+        int tallest = 0;
+        int talCount = 0;
+        
+        foreach(var candNum in candles)
+        {
+            if(tallest < candNum)
+            {
+                tallest = candNum;
+                talCount = 0;
+            }
+            
+            if(tallest == candNum)
+                talCount++;
+        }
+        
+        return talCount;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int candlesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> candles = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(candlesTemp => Convert.ToInt32(candlesTemp)).ToList();
+
+        int result = Result.birthdayCakeCandles(candles);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
