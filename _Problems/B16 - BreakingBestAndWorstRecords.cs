4882f888eccead2dbd03d34b81f15a6c590dec50
Gabriel Hortmann <gabrielhortmann@gmail.com>
2025-08-30 13:25:32 -0300

    www.hackerrank.com/challenges/breaking-best-and-worst-records/problem

---
+++ _Problems/B16 - BreakingBestAndWorstRecords.cs
@@ -0,0 +1,64 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'breakingRecords' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY scores as parameter.
+     */
+
+    public static List<int> breakingRecords(List<int> scores)
+    {
+        int max = scores[0], min = scores[0], maxQtd = 0, minQtd = 0;
+        foreach(var score in scores)
+        {
+            if(max < score)
+            {
+                maxQtd++;   
+                max = score;
+            }
+            if(min > score)
+            {
+                minQtd++;
+                min = score;
+            }
+        }
+        
+        return new List<int> { maxQtd, minQtd};
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> scores = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(scoresTemp => Convert.ToInt32(scoresTemp)).ToList();
+
+        List<int> result = Result.breakingRecords(scores);
+
+        textWriter.WriteLine(String.Join(" ", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
