65c7d77f32c910a8a9e4689ad85097a0611c576f
hortty <gabrielhortmann@gmail.com>
2026-05-26 15:53:44 -0300

    www.hackerrank.com/challenges/counting-valleys/problem

---
+++ _Problems/C23 - CountingValleys.cs
@@ -0,0 +1,68 @@
+// www.hackerrank.com/challenges/counting-valleys/problem
+
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'countingValleys' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER steps
+     *  2. STRING path
+     */
+
+    public static int countingValleys(int steps, string path)
+    {
+        int valleySeq = 0;
+        int currentDirection = 0;
+        foreach(char action in path)
+        {
+            if(action == 'D')
+            {
+                currentDirection--;
+                if(currentDirection == -1)
+                    valleySeq++;
+            }
+            else
+            {
+                currentDirection++;
+            }
+        }
+        return valleySeq;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int steps = Convert.ToInt32(Console.ReadLine().Trim());
+
+        string path = Console.ReadLine();
+
+        int result = Result.countingValleys(steps, path);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
